be70b4f4c203a04cea2c38c606dba17fef2f26ce
Ruben <info@blogruben.com>
2023-05-24 13:14:21 +0200

    IO___leer_escribir_fichero: crear fichero temporal

---
+++ IO__leer_escribir_ficheros/src/main/java/org/ruben/ficheros/escribir/CrearFicheroTemporal.java
@@ -0,0 +1,61 @@
+package org.ruben.ficheros.escribir;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CrearFicheroTemporal {
+    public static void main(String[] args) {
+        crearficherotemporal();
+        crearficherotemporalConRuta();
+    }
+
+
+    public static void crearficherotemporal() {
+        try {
+            // Genera el fichero temporal es un texto aleatori con prefijos y sufijos
+            // Si el sufijo es null, le pone “.tmp”.
+            File tempFile = File.createTempFile("mificherotemporal", null); // mificherotemporal8803114360681798207.tmp
+
+            // Permite que el fichero se borre al salir del programa
+            tempFile.deleteOnExit();
+
+            BufferedWriter out = new BufferedWriter(new FileWriter(tempFile));
+
+            out.write("Esto es un fichero temporal");
+            out.close();
+
+            mostrarRuta(tempFile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void crearficherotemporalConRuta() {
+        try {
+            File tempFile = File.createTempFile("pruebaRUben",
+                    ".txt",
+                    new File("C:/Users/99GU8572/Desktop"));
+            tempFile.deleteOnExit();
+            mostrarRuta(tempFile);
+
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    private static void mostrarRuta(File tempFile) {
+        // check if the file is created
+        if (tempFile.exists()) {
+            System.out.println("Fichero creado: "
+                    + tempFile.getAbsolutePath());
+        } else {
+            System.out.println("No se ha podido crear: "
+                    + tempFile.getAbsolutePath());
+        }
+    }
+
+}
