c03e9b9ccfd94341edc84bc6732fb19ba0802e37
Ruiz Carbonell <ruben.ruiz-carbonell@siemens.com>
2024-02-13 11:12:02 +0100

    Delete files .history

--- JAVA__manejar_texto/.history/src/main/java/org/ruben/java/leer/string/Replace_20230522174658.java
+++
@@ -1,7 +0,0 @@
-package org.ruben.java.leer.string;
-
-public class Replace {
-    String myStr = "Hello";
-System.out.println(myStr.replace('l', 'p'));
-
-}
--- JAVA__manejar_texto/.history/src/main/java/org/ruben/java/leer/string/StartsWith_20230522172142.java
+++
@@ -1,8 +0,0 @@
-package org.ruben.java.leer.string;
-
-public class App {
-    
-
-    https://www.baeldung.com/string/starts-with
-
-}
